0c4754893ba84213e017ef0524c503a1f04fb8ee
curtis <curtis@8f095f08-0861-0410-be38-c3242cf3441a>
2009-06-30 03:51:40 +0000

    Feeder is now protected against IOErrors. It displays descriptions w/ smart truncation.
    git-svn-id: http://svn.lassam.net/pierc@49 8f095f08-0861-0410-be38-c3242cf3441a

--- bot/Feeder.py
+++ bot/Feeder.py
@@ -1,6 +1,13 @@
 import httplib
 import BeautifulSoup
 
+# Stolen from Adam, from Stack Overflow. Thanks Adam!		
+def smart_truncate(content, length=100, suffix='...'):
+    if len(content) <= length:
+        return content
+    else:
+        return ' '.join(content[:length+1].split(' ')[0:-1]) + suffix	
+
 class Feeder: 
 	def __init__(self):
 		feed = "feed://pipes.yahoo.com/pipes/pipe.run?_id=8L8S0Ppk3hG2rnwcqevxTA&_render=rss"
@@ -11,30 +18,43 @@ class Feeder:
 			self.already_viewed.append( x.title.string )
 		
 	def get_feed(self):
-		self.conn.request("GET", "/pipes/pipe.run?_id=8L8S0Ppk3hG2rnwcqevxTA&_render=rss")
-		r1 = self.conn.getresponse()
-		xml = BeautifulSoup.BeautifulStoneSoup(r1);
-		return xml
+		try:
+			self.conn.request("GET", "/pipes/pipe.run?_id=8L8S0Ppk3hG2rnwcqevxTA&_render=rss")
+			r1 = self.conn.getresponse()
+			xml = BeautifulSoup.BeautifulStoneSoup(r1);
+			return xml
+		except:
+			return False
 		
 	def update(self):
-		xml = self.get_feed()
-		new = []
-		for x in xml('item'):
-			if not x.title.string in self.already_viewed:
-				self.already_viewed.append( x.title.string )
-				new.append( x.title.string + " --> " + x.link.string  )
-		return new;
+		try:
+			xml = self.get_feed()
+			if not xml:
+				return False
+			new = []
+			for x in xml('item'):
+				if not x.title.string in self.already_viewed:
+					self.already_viewed.append( x.title.string )
+					new.append( x.title.string + " --> " + x.link.string + " :: " + smart_truncate(x.description.string)  )
+			return new;
+		except:
+			return [];
 	
 	def top5(self):
-		xml = self.get_feed()
-		new = []
-		counter = 0
-		for x in xml('item'):
-			new.append( x.title.string + " --> " + x.link.string  )
-			counter += 1
-			if counter == 5:
-				break
-		return new;
+		try:
+			xml = self.get_feed()
+			if not xml:
+				return False
+			new = []
+			counter = 0
+			for x in xml('item'):
+				new.append( x.title.string + " --> " + x.link.string + " :: " + smart_truncate(x.description.string)  )
+				counter += 1
+				if counter == 5:
+					break
+			return new;
+		except:
+			return new;	
 		
 if __name__ == "__main__":
 	fdr = Feeder()
