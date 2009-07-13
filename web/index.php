9617c57e0a0a70a6ea48e4d0370620f635a4f91b
curtis <curtis@8f095f08-0861-0410-be38-c3242cf3441a>
2009-07-13 23:33:24 +0000

    Changed "autoboose.js" to "LumberJack_js_app.js" because I hate life.
    git-svn-id: http://svn.lassam.net/pierc@55 8f095f08-0861-0410-be38-c3242cf3441a

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@
 	
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"> </script>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/jquery-ui.min.js"> </script>
-	<script type="text/javascript" src="autoboose.js"> </script>
+	<script type="text/javascript" src="LumberJack_js_app.js"> </script>
 
 </head>
 <body>
