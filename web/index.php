26e2df770207d7d8bf8b643e53145e6498eea31d
Curtis Lassam <curtis@lassam.net>
2012-11-23 11:18:31 -0800

    Issue #1

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@
 	
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"> </script>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/jquery-ui.min.js"> </script>
-	<script type="text/javascript" src="pierc_js.js"> </script>
+	<script type="text/javascript" src="pierc.js"> </script>
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
 
 </head>
